67729497160cb1dfe207761a785d7fd9c395821f
sowmyas25082003 <s98388510@gmail.com>
2026-08-18 12:07:40 +0530

    Configure RDS database connection

--- database.py
+++ database.py
@@ -8,7 +8,7 @@ load_dotenv()
 def get_connection():
     return mysql.connector.connect(
         host=os.getenv("DB_HOST"),
-        port=3306,
+        port=int(os.getenv("DB_PORT", 3306)),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
         database=os.getenv("DB_NAME"),
